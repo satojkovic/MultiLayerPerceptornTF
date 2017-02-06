24d101fce5e5a345bcec2cf6cdf1f51a1e55ed43
satojkovic <satojkovic@gmail.com>
2017-02-06 22:50:54 +0900

    Load digits data

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -7,8 +7,22 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 
 
+def one_hot_encoding(target):
+    n_classes = np.max(target) + 1
+    one_hot = [
+        list((np.arange(n_classes) == t).astype(np.int)) for t in target
+    ]
+    return np.array(one_hot)
+
+
 def main():
-    pass
+    digits = load_digits()
+    X_train, X_test, y_train, y_test = train_test_split(
+        digits.data, one_hot_encoding(digits.target))
+    print('Train data: {}'.format(X_train.shape))
+    print('Train labels: {}'.format(y_train.shape))
+    print('Test data: {}'.format(X_test.shape))
+    print('Test labels: {}'.format(y_test.shape))
 
 
 if __name__ == '__main__':
