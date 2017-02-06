ae6b41f0a2d7d310b4906a12a2922e2636b0e66a
satojkovic <satojkovic@gmail.com>
2017-02-06 22:39:57 +0900

    Add a new file

---
+++ multi_layer_perceptron.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
