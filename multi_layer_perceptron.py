c8b89180c0f25f34db3e5b7ee6122e4c7bf22c13
satojkovic <satojkovic@gmail.com>
2017-02-07 23:21:20 +0900

    Use MNIST dataset

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
 import tensorflow as tf
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-
-
-def one_hot_encoding(target):
-    n_classes = np.max(target) + 1
-    one_hot = [
-        list((np.arange(n_classes) == t).astype(np.int)) for t in target
-    ]
-    return np.array(one_hot)
+from tensorflow.examples.tutorials.mnist import input_data
 
 
 def main():
-    digits = load_digits()
-    X_train, X_test, y_train, y_test = train_test_split(
-        digits.data, one_hot_encoding(digits.target))
-    print('Train data: {}'.format(X_train.shape))
-    print('Train labels: {}'.format(y_train.shape))
-    print('Test data: {}'.format(X_test.shape))
-    print('Test labels: {}'.format(y_test.shape))
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    print('Train data: {}'.format(mnist.train.images.shape))
+    print('Train labels: {}'.format(mnist.train.labels.shape))
+    print('Test data: {}'.format(mnist.test.images.shape))
+    print('Test labels: {}'.format(mnist.test.labels.shape))
 
 
 if __name__ == '__main__':
